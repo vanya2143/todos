1b302eda23d7702e32f0cee5d3f8c18a8ded71b4
Ivan Bielozorov <vanya2143@gmail.com>
2021-03-10 18:46:56 +0200

    Add api-root url

--- todos/urls.py
+++ todos/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path, include
+from django.urls import path
 
-from .views import TodoList, TodoDetail, UserList, UserDetail
-
+from .views import TodoList, TodoDetail, UserList, UserDetail, ApiRoot
 
 urlpatterns = [
+    path('', ApiRoot.as_view(), name='api-root'),
+
     path('users/', UserList.as_view(), name='user-list'),
     path('users/<int:pk>', UserDetail.as_view(), name='user-detail'),
-    path('<int:pk>', TodoDetail.as_view(), name='todo-detail'),
-    path('', TodoList.as_view(), name='todo-list'),
+
+    path('todos/', TodoList.as_view(), name='todo-list'),
+    path('todos/<int:pk>', TodoDetail.as_view(), name='todo-detail'),
 ]
