61b794e84bc06102aa7b0dfd657880d3c7a78dca
Ivan Bielozorov <vanya2143@gmail.com>
2021-03-10 18:45:38 +0200

    Create permission class IsOwnerOrAdmin

--- todos/permissions.py
+++ todos/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 
 
-class IsOwner(permissions.BasePermission):
+class IsOwnerOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.owner == request.user
+        return (obj.owner == request.user) or (request.user and request.user.is_staff)
