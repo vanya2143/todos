b4157e6b1d1b76f99338fd240de73cfdfa996b48
Ivan Bielozorov <vanya2143@gmail.com>
2021-03-10 19:46:47 +0200

    Remove token auth

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
     'todos',
 ]
 
@@ -123,7 +122,6 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
