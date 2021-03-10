1a1207765b89c20b65123b6c4c146d65f36b7511
Ivan Bielozorov <vanya2143@gmail.com>
2021-03-10 20:36:56 +0200

    Add jwt auth app

--- auth/urls.py
+++ auth/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 
+from .views import RegisterView
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='auth_register')
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,4 +1,13 @@
+from django.contrib.auth.models import User
+
+from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+from .serializers import RegistrationSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegistrationSerializer
 
