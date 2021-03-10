ebe5538bcb3e48501355869d946b3caf45680c0d
Ivan Bielozorov <vanya2143@gmail.com>
2021-03-10 19:47:20 +0200

    Add jwt auth

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,7 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
