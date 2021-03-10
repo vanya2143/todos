2d2e76925beb85072e64cd03f7c9e522e9a625f2
Ivan Bielozorov <vanya2143@gmail.com>
2021-03-10 19:46:19 +0200

    Remove token auth

--- config/urls.py
+++ config/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('todos.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('api-token-auth/', views.obtain_auth_token),
+    # path('api-token-auth/', views.obtain_auth_token),
 ]
