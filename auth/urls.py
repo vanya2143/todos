a1ddee754616eea3d63abf8cf43d8a2c8f8a1fd0
Ivan Bielozorov <vanya2143@gmail.com>
2021-03-10 21:06:43 +0200

    Change token url name from 'login' to 'token'

--- auth/urls.py
+++ auth/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views import RegisterView
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='auth_register')
 ]
