3d1f7810e39ae8af84d8208e154e87eb9504a4a7
Ivan Bielozorov <vanya2143@gmail.com>
2021-03-10 20:43:39 +0200

    Move auth/ to api/auth

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('todos.urls')),
+    path('api/auth/', include('auth.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('auth/', include('auth.urls')),
 ]
