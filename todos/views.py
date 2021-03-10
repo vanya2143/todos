3f68e69928ef279fa867a14fd1b0d25b31be831b
Ivan Bielozorov <vanya2143@gmail.com>
2021-03-10 18:47:43 +0200

    Add api-root view Fix TodoList Add permissions for all views

--- todos/views.py
+++ todos/views.py
@@ -1,66 +1,54 @@
 from django.contrib.auth.models import User
 
 from rest_framework.views import APIView
-from rest_framework import mixins
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
+from rest_framework.routers import reverse
 
 from .models import Todo
 from .serializers import TodoSerializer, UserSerializer
-from .permissions import IsOwner
+from .permissions import IsOwnerOrAdmin
 
 
-# class TodoList(mixins.CreateModelMixin, generics.GenericAPIView):
-#     permission_classes = [permissions.IsAuthenticated]
-#     serializer_class = TodoSerializer
-#     queryset = Todo.objects.all()
-#
-#     def get(self, request):
-#         try:
-#             todos = self.queryset.filter(owner=request.user)
-#         except Todo.DoesNotExist:
-#             todos = []
-#         serializer = TodoSerializer(todos, context={'request': request}, many=True)
-#         return Response(serializer.data)
-#
-#     def post(self, request, *args, **kwargs):
-#         print(request.user, request.data)
-#         self.create(request, *args, **kwargs)
+class ApiRoot(APIView):
+    def get(self, request):
+        return Response({
+            'users': reverse('user-list', request=request),
+            'todos': reverse('todo-list', request=request)
+        })
+
 
 class TodoList(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
     def get(self, request):
-        todos = Todo.objects.all()
-        serializer = TodoSerializer(todos, many=True)
+        todos = Todo.objects.all().filter(owner=request.user)
+        serializer = TodoSerializer(todos, many=True, context={'request': request})
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = TodoSerializer(data=request.data)
-        print(request.data)
+        serializer = TodoSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
-            serializer.save()
+            serializer.save(owner=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# class TodoList(generics.ListCreateAPIView):
-#     queryset = Todo.objects.all()
-#     serializer_class = TodoSerializer
-
 
 class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
-    # permission_classes = (permissions.IsAuthenticated, IsOwner)
+    permission_classes = [permissions.IsAuthenticated & IsOwnerOrAdmin]
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
 
 
 class UserList(generics.ListAPIView):
-    # permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (permissions.IsAdminUser,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class UserDetail(generics.RetrieveAPIView):
-    # permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (permissions.IsAdminUser,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
